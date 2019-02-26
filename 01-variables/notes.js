379fb224999b87698bb5c5717ce1fdc2cb4f145f
BadNews87 <krisu876@wp.pl>
2019-02-26 20:43:36 +0100

    Burrito exercise done

--- 01-variables/notes.js
+++ 01-variables/notes.js
@@ -8,3 +8,14 @@ console.log(name);
 var greeting = "Hello " + name;
 
 console.log(greeting);
+
+// Burritos
+
+var burritoAmount = 5;
+var burritoPrice = 5.99;
+var taxRate = 0.23;
+var taxAmount = burritoPrice * taxRate;
+var totalBurritoPrice = burritoPrice + taxAmount;
+var totalPrice = burritoAmount * totalBurritoPrice;
+
+console.log("The total price for 5 burritos is", totalPrice);
