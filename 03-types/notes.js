68e4f005e0f468299ddfe9938a0d6f46ebbb6da4
BadNews87 <krisu876@wp.pl>
2020-12-10 20:50:56 +0100

    Types update

--- 03-types/notes.js
+++ 03-types/notes.js
@@ -1,2 +1,26 @@
 // your notes here!
 console.log("hello world!");
+
+var celsiusToFahrenheit = function (celsiusValue) {
+    return (celsiusValue * 9) / 5 + 32;
+};
+
+console.log(celsiusToFahrenheit(0));
+
+
+var burritoTotalPrice = function (price, taxRate) {
+    var taxAmount = price * taxRate;
+    var total = price + taxAmount;
+    return Math.round(total * 100) / 100;
+};
+
+console.log(burritoTotalPrice(5.99, 0.09));
+
+
+var tweet = "Hey my name is Slim Shady";
+
+var lastThreeCapitalized = function (text) {
+    return text.slice(text.length - 3).toUpperCase();
+};
+
+console.log(lastThreeCapitalized(tweet));
